7da7b5900a2c824d9f4b82a473942697353e910e
vivek248-cloud <vivekofficial248@gmail.com>
2025-05-31 12:30:12 +0530

    Create create_superuser.py

---
+++ ledger_system/create_superuser.py
@@ -0,0 +1,20 @@
+# create_superuser.py
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ledger_system.settings")
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = "admin"
+email = "admin@example.com"
+password = "Admin123"
+
+if not User.objects.filter(username=username).exists():
+    print("Creating superuser...")
+    User.objects.create_superuser(username=username, email=email, password=password)
+else:
+    print("Superuser already exists.")
