b975b2c4aba5c7644cc790aaa0f93389d91e800a
vivek248-cloud <vivekofficial248@gmail.com>
2025-06-10 13:05:25 +0530

    Update models.py

--- ledger_system/projects/models.py
+++ ledger_system/projects/models.py
@@ -137,3 +137,11 @@ class DailyExpense(models.Model):
 
 
     
+class SiteImage(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='site_images')
+    image = models.ImageField(upload_to='site_images/')
+    description = models.TextField(blank=True, null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Image for {self.project.client_name}"
