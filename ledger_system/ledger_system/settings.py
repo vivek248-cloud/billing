d1f3bc78f75d58ef23a3397bc9deadc3dc799433
vivek248-cloud <vivekofficial248@gmail.com>
2025-07-05 20:03:37 +0530

    Update settings.py

--- ledger_system/ledger_system/settings.py
+++ ledger_system/ledger_system/settings.py
@@ -2,7 +2,9 @@
 
 import os
 from pathlib import Path
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR == /root/edb/billing/ledger_system
+
+# BASE_DIR = Path(__file__).resolve().parent.parent
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'
