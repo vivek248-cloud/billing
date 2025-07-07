20079f22390d1d78dd88e3bd7e09b61e5bf72e5c
Vivek248-cloud <vivekofficial248.com>
2025-07-07 15:21:12 +0530

    Fix static files and sync project

--- ledger_system/wsgi.py
+++ ledger_system/wsgi.py
@@ -9,12 +9,6 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise  # ✅ import WhiteNoise
-from pathlib import Path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ledger_system.settings')
-
 application = get_wsgi_application()
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))  # ✅ Add this line
